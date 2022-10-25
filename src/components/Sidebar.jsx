2bd1be248b93249ff453906b75c868ac06054823
Tiago Leite <tiagoleite1405@gmail.com>
2022-10-25 13:24:18 -0300

    Fix sidebar icon alignment

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -24,7 +24,7 @@ const Sidebar = () => {
       </li>
       <li className="flex justify-between items-between w-40 h-14 ml-[-100px] hover:ml-0 duration-300 bg-[#1B1F23]">
         <a
-          className="flex justify-around items-center w-full "
+          className="flex justify-around items-center w-full"
           href={social.github}
           target="_blank"
           rel="noreferrer"
@@ -34,15 +34,15 @@ const Sidebar = () => {
       </li>
       <li className="flex justify-between items-between w-40 h-14 ml-[-100px] hover:ml-0 duration-300 bg-[#e22211]">
         <a
-          className="flex justify-around items-center w-full "
+          className="flex justify-around items-center w-full"
           href={social.email}
         >
-          Email <HiOutlineMail size={30} />
+          Email <HiOutlineMail className="ml-4" size={30} />
         </a>
       </li>
       <li className="flex justify-between items-between w-40 h-14 ml-[-100px] hover:ml-0 duration-300 bg-[#4E535E]">
         <a
-          className="flex justify-around items-center w-full "
+          className="flex justify-around items-center w-full"
           href="/"
           target="_blank"
           rel="noreferrer"

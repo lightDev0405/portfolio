2134c5c8b5056db64184b367ded1140ab8359128
Tiago Leite <tiagoleite1405@gmail.com>
2023-01-10 11:19:14 -0300

    Add back-end skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,17 +5,21 @@ import {
   SiJavascript,
   SiReact,
   SiFigma,
+  SiExpress,
+  SiMongodb,
 } from 'react-icons/si';
 import { DiGit } from 'react-icons/di';
-import { i18n } from '../locale/i18n';
 import { BsBootstrapFill, BsPlusLg } from 'react-icons/bs';
+import { FaNodeJs, FaSass } from 'react-icons/fa';
+import { HiMinus } from 'react-icons/hi';
+
+import { i18n } from '../locale/i18n';
 import { useState } from 'react';
 
 const Skills = () => {
   const [active, setActive] = useState(false);
   const showMore = () => {
     setActive(!active);
-    console.log(active);
   };
 
   return (
@@ -30,42 +34,34 @@ const Skills = () => {
           </h2>
         </div>
         <div className="w-full grid grid-cols-2 sm:grid-cols-4 text-center gap-5 py-10">
-          <div className="order-1 shadow-md shadow-[rgba(19,33,50,0.8)] p-5 hover:scale-[1.2] duration-500 select-none">
+          <div className="shadow-md shadow-[rgba(19,33,50,0.8)] p-5 hover:scale-[1.2] duration-500 select-none">
             <SiReact className="text-[#61DAFB] mx-auto mb-4 text-4xl md:text-6xl" />
             <span className="mt-4 text-sm md:text-base">React</span>
           </div>
-          <div
-            className={`${
-              active ? 'block' : 'hidden'
-            } sm:block order-7 sm:order-3 shadow-md shadow-[rgba(19,33,50,0.8)] p-5 hover:scale-[1.2] duration-500 select-none`}
-          >
-            <BsBootstrapFill className="text-[#69419C] mx-auto mb-4 text-4xl md:text-6xl" />
-            <span className="mt-4 text-sm md:text-base">Bootstrap</span>
+          <div className="shadow-md shadow-[rgba(19,33,50,0.8)] p-5 hover:scale-[1.2] duration-500 select-none">
+            <FaNodeJs className="text-[#8BBF3D] mx-auto mb-4 text-4xl md:text-6xl" />
+            <span className="mt-4 text-sm md:text-base">NodeJs</span>
+          </div>
+          <div className="shadow-md shadow-[rgba(19,33,50,0.8)] p-5 hover:scale-[1.2] duration-500 select-none">
+            <SiExpress className="text-[#EEEEEE] mx-auto mb-4 text-4xl md:text-6xl" />
+            <span className="mt-4 text-sm md:text-base">Express</span>
+          </div>
+          <div className="shadow-md shadow-[rgba(19,33,50,0.8)] p-5 hover:scale-[1.2] duration-500 select-none">
+            <SiMongodb className="text-[#0FA54D] mx-auto mb-4 text-4xl md:text-6xl" />
+            <span className="mt-4 text-sm md:text-base">MongoDB</span>
           </div>
           <div
             className={`${
               active ? 'block' : 'hidden'
-            } sm:block order-6 sm:order-2 shadow-md shadow-[rgba(19,33,50,0.8)] p-[20px] hover:scale-[1.2] duration-500 select-none`}
+            } sm:block shadow-md shadow-[rgba(19,33,50,0.8)] p-5 hover:scale-[1.2] duration-500 select-none`}
           >
-            <SiTailwindcss className="text-[#36B7F0] mx-auto mb-4 text-4xl md:text-6xl" />
-            <span className="mt-4 text-sm md:text-base">Tailwind CSS</span>
-          </div>
-          <div className="order-2 sm:order-4 shadow-md shadow-[rgba(19,33,50,0.8)] p-5 hover:scale-[1.2] duration-500 select-none">
-            <DiGit className="scale-[1.4] text-[#e44c30] mx-auto mb-4 text-4xl md:text-6xl" />
-            <span className="mt-4 text-sm md:text-base">Git</span>
-          </div>
-          <div className="order-3 sm:order-5 shadow-md shadow-[rgba(19,33,50,0.8)] p-5 hover:scale-[1.2] duration-500 select-none">
             <SiJavascript className="text-[#F0D506] mx-auto mb-4 text-4xl md:text-6xl" />
             <span className="mt-4 text-sm md:text-base">ES6</span>
           </div>
-          <div className="order-4 sm:order-7 shadow-md shadow-[rgba(19,33,50,0.8)] p-5 hover:scale-[1.2] duration-500 select-none">
-            <SiCss3 className="text-[#254BDD] mx-auto mb-4 text-4xl md:text-6xl" />
-            <span className="mt-4 text-sm md:text-base">CSS3</span>
-          </div>
           <div
             className={`${
               active ? 'block' : 'hidden'
-            } sm:block order-5 sm:order-6 shadow-md shadow-[rgba(19,33,50,0.8)] p-5 hover:scale-[1.2] duration-500 select-none`}
+            } sm:block shadow-md shadow-[rgba(19,33,50,0.8)] p-5 hover:scale-[1.2] duration-500 select-none`}
           >
             <SiHtml5 className="text-[#DD4B25] mx-auto mb-4 text-4xl md:text-6xl" />
             <span className="mt-4 text-sm md:text-base">HTML5</span>
@@ -73,20 +69,64 @@ const Skills = () => {
           <div
             className={`${
               active ? 'block' : 'hidden'
-            } sm:block order-8 shadow-md shadow-[rgba(19,33,50,0.8)] p-5 hover:scale-[1.2] duration-500 select-none`}
+            } sm:block shadow-md shadow-[rgba(19,33,50,0.8)] p-5 hover:scale-[1.2] duration-500 select-none`}
+          >
+            <SiCss3 className="text-[#254BDD] mx-auto mb-4 text-4xl md:text-6xl" />
+            <span className="mt-4 text-sm md:text-base">CSS3</span>
+          </div>
+          <div
+            className={`${
+              active ? 'block' : 'hidden'
+            } sm:block shadow-md shadow-[rgba(19,33,50,0.8)] p-5 hover:scale-[1.2] duration-500 select-none`}
+          >
+            <DiGit className="scale-[1.4] text-[#e44c30] mx-auto mb-4 text-4xl md:text-6xl" />
+            <span className="mt-4 text-sm md:text-base">Git</span>
+          </div>
+          <div
+            className={`${
+              active ? 'block' : 'hidden'
+            } sm:block shadow-md shadow-[rgba(19,33,50,0.8)] p-5 hover:scale-[1.2] duration-500 select-none`}
+          >
+            <FaSass className="scale-[1.4] text-[#C66394] mx-auto mb-4 text-4xl md:text-6xl" />
+            <span className="mt-4 text-sm md:text-base">Sass</span>
+          </div>
+          <div
+            className={`${
+              active ? 'block' : 'hidden'
+            } sm:block shadow-md shadow-[rgba(19,33,50,0.8)] p-5 hover:scale-[1.2] duration-500 select-none`}
+          >
+            <BsBootstrapFill className="text-[#69419C] mx-auto mb-4 text-4xl md:text-6xl" />
+            <span className="mt-4 text-sm md:text-base">Bootstrap</span>
+          </div>
+          <div
+            className={`${
+              active ? 'block' : 'hidden'
+            } sm:block shadow-md shadow-[rgba(19,33,50,0.8)] p-[20px] hover:scale-[1.2] duration-500 select-none`}
+          >
+            <SiTailwindcss className="text-[#36B7F0] mx-auto mb-4 text-4xl md:text-6xl" />
+            <span className="mt-4 text-sm md:text-base">Tailwind CSS</span>
+          </div>
+          <div
+            className={`${
+              active ? 'block' : 'hidden'
+            } sm:block shadow-md shadow-[rgba(19,33,50,0.8)] p-5 hover:scale-[1.2] duration-500 select-none`}
           >
             <SiFigma className="text-[#62CA80] mx-auto mb-4 text-4xl md:text-6xl" />
             <span className="mt-4 text-sm md:text-base">Figma</span>
           </div>
         </div>
         <button
-          className={`${
-            active ? 'hidden' : 'block'
-          } sm:hidden w-36 h-11 flex justify-center items-center mx-auto rounded bg-transparent border-2 hover:text-white active:text-white hover:bg-theme-blue-50 active:bg-theme-blue-50 hover:border-theme-blue-50 active:border-theme-blue-50 duration-200 group font-medium`}
+          className="sm:hidden w-36 h-11 flex justify-center items-center mx-auto rounded bg-transparent border-2 hover:text-white active:text-white hover:bg-theme-blue-50 active:bg-theme-blue-50 hover:border-theme-blue-50 active:border-theme-blue-50 duration-200 group font-medium"
           onClick={showMore}
         >
-          {i18n.t('skillsSection.button')}
-          <BsPlusLg className="ml-1" />
+          {active
+            ? `${i18n.t('skillsSection.showLess')}`
+            : `${i18n.t('skillsSection.showMore')}`}
+          {active ? (
+            <HiMinus className="ml-1" />
+          ) : (
+            <BsPlusLg className="ml-1" />
+          )}
         </button>
       </div>
     </section>

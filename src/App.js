c0f8d963002c1a45c01ece9106780733c2912257
Tiago Leite <tiagoleite1405@gmail.com>
2023-01-11 13:25:56 -0300

    Add Toaster, react notifications

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <>
+      <Toaster />
       <Header />
       <Main />
       <Footer />

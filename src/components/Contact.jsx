74dfc5e5332d20df2e7293395102ae328ee0e29f
Tiago Leite <tiagoleite1405@gmail.com>
2023-01-11 13:32:09 -0300

    Refactor contact form, add EmailJs email sender

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,33 @@
 import { RiSendPlaneFill } from 'react-icons/ri';
 import { i18n } from '../locale/i18n';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import toast from 'react-hot-toast';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_EMAILJS_SERVICE_ID,
+        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        () => {
+          e.target.reset();
+          toast.success(i18n.t('toast.contact.success'), { duration: 5000 });
+        },
+        (error) => {
+          toast.error(i18n.t('toast.contact.error'), { duration: 5000 });
+        }
+      );
+  };
+
   return (
     <section className="w-full pb-5 md:pb-9" name="contact">
       <div className="max-w-[900px] mx-auto px-8">
@@ -13,8 +39,8 @@ const Contact = () => {
         </p>
         <form
           className="flex flex-col w-full max-w-[600px] mx-auto pt-12"
-          action="https://getform.io/f/8010a764-5271-4d08-b8af-58a7d3654b1e"
-          method="POST"
+          ref={form}
+          onSubmit={sendEmail}
         >
           <input
             className="p-2 md:p-2.5 rounded-sm bg-theme-white text-theme-black text-lg outline-none"
@@ -22,14 +48,14 @@ const Contact = () => {
             placeholder={`${i18n.t(
               'contactSection.placeholders.placeholderName'
             )}`}
-            name="name"
+            name="user_name"
             required
           />
           <input
             className="p-2 md:p-2.5 my-3 md:my-4 rounded-sm bg-theme-white text-theme-black text-lg outline-none"
             type="email"
             placeholder="Email"
-            name="email"
+            name="user_email"
             required
           ></input>
           <textarea
@@ -41,7 +67,10 @@ const Contact = () => {
             rows="7"
             required
           ></textarea>
-          <button className="w-28 h-11 md:w-32 md:h-12 flex items-center justify-center cursor-pointer hover:text-white active:text-white duration-200 bg-theme-blue-50 hover:bg-theme-blue-100 active:bg-theme-blue-100 rounded text-lg md:text-xl mt-3 md:mt-5 mx-auto font-medium">
+          <button
+            type="submit"
+            className="w-28 h-11 md:w-32 md:h-12 flex items-center justify-center cursor-pointer hover:text-white active:text-white duration-200 bg-theme-blue-50 hover:bg-theme-blue-100 active:bg-theme-blue-100 rounded text-lg md:text-xl mt-3 md:mt-5 mx-auto font-medium"
+          >
             {i18n.t('contactSection.button')}{' '}
             <RiSendPlaneFill className="ml-3" />
           </button>

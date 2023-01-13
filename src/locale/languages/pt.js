db8077138af362f74ec555c659b0168aa5b6a150
Tiago Leite <tiagoleite1405@gmail.com>
2023-01-13 12:53:03 -0300

    Update portuguese locale texts

--- src/locale/languages/pt.js
+++ src/locale/languages/pt.js
@@ -20,12 +20,14 @@ const content = {
           textOne: 'programador full-stack',
           textTwo: 'desenvolvedor web',
           textThree: 'programador freelancer',
-          textFour: 'full-stack MERN',
+          textFour: 'mago do CSS',
           textFive: 'desenvolvedor React',
-          textSix: 'programador JavaScript',
+          textSix: 'ninja do JavaScript',
+          textSeven: 'engenheiro front-end',
         },
+        aboutMe: 'Sobre mim',
         description:
-          'Sou um desenvolvedor front-end autodidata com uma variedade de projetos incríveis. Eu me dedico a desenvolver sites e aplicativos web que sejam objetivos, funcionais e atraentes em seu visual. Sempre baseado nos princípios de usabilidade e navegabilidade.',
+          'Sou um desenvolvedor web autodidata e motivado, estou constantemente aprendendo novas tecnologias. Meu principal foco é desenvolver sites e aplicativos web que sejam rápidos, funcionais e atraentes em seu visual. Sempre me baseando nos princípios de usabilidade e navegabilidade.',
         buttons: {
           work: 'Projetos',
           hire: 'Contato',
@@ -34,14 +36,14 @@ const content = {
       skillsSection: {
         sectionName: 'Skills',
         intro:
-          'Algumas das principais tecnologias que estou usando atualmente em meu conjunto de ferramentas.',
+          'Comecei a estudar construindo uma base sólida nos princípios do desenvolvimento web, HTML, CSS, Javascript e acessibilidade. Aprendi frameworks front-end e back-end. Me apronfundei em ferramentas de indústria como o Git e boas práticas de commits, ferramentas de teste como Jest e Mocha, linting, gerenciamento de pacotes, e ferramentas de construção como Webpack e Babel, dentre muitas outras. Fiz um pequeno compilado das principais tecnologias que estou usando atualmente em meu conjunto de ferramentas. Eu estou sempre aberto a aprender novas tecnologias para expandir meu conhecimento.',
         showMore: 'Mostrar Mais',
         showLess: 'Esconder',
       },
       worksSection: {
         sectionName: 'Projetos',
         intro:
-          'Criei e participei do desenvolvimento de projetos completos. realizando o desenvolvimento de suas bases de código front-end e colaborando com designers. Aqui estão alguns exemplos do meu trabalho.',
+          'Criei e participei do desenvolvimento de projetos completos. realizando o desenvolvimento de suas bases de código desde o front-end até o back-end, testando, otimizando, corrigindo bugs e colaborando com designers. Aqui estão alguns exemplos do meu trabalho.',
         workElement: {
           tools: 'Ferramentas',
           buttons: {
@@ -79,7 +81,7 @@ const content = {
       contactSection: {
         sectionName: 'Contato',
         intro:
-          'Para entrar em contato comigo, preencha o formulário abaixo, ou se preferir, entre em contato pelo LinkedIn ou envie um e-mail. Estou disponível para contratos e aberto a qualquer idéia de cooperação.',
+          'Para entrar em contato comigo, preencha o formulário abaixo, ou se preferir, entre em contato pelo LinkedIn ou envie um e-mail. Estou disponível para contratos e aberto a qualquer idéia de cooperação, incluindo freelancing.',
         placeholders: {
           placeholderName: 'Nome',
           placeholderMessage: 'Mensagem',
@@ -92,6 +94,7 @@ const content = {
           error: 'Erro! Por favor, tente novamente mais tarde.',
         },
       },
+      footer: 'fonte',
     },
   },
 };

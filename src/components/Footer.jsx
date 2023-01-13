4c9c19dd8cc5d9f33fb3c439da51ca58b71a49a1
Tiago Leite <tiagoleite1405@gmail.com>
2023-01-13 12:51:27 -0300

    Change font sizes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,8 @@
+import { i18n } from '../locale/i18n';
+
 const Footer = () => {
   return (
-    <footer className="w-full h-8 md:h-9 bg-[#262629] text-theme-white flex justify-center items-center shadow-[0px_-1px_10px_0px] shadow-[rgba(0,0,0,0.2)]">
+    <footer className="w-full h-8 2xl:h-9 bg-[#262629] text-theme-white flex justify-center items-center shadow-[0px_-1px_10px_0px] shadow-[rgba(0,0,0,0.2)]">
       <span className="text-sm font-medium">2020 Tiago Leite</span>{' '}
       <a
         className="text-sm absolute right-2.5"
@@ -8,7 +10,7 @@ const Footer = () => {
         target="_blank"
         rel="nofererrer"
       >
-        source
+        {i18n.t('footer')}
       </a>
     </footer>
   );

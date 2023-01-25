c2a3fa01f5105f153e2c28fd719a29a2cc15fbf9
Tiago Leite <contato@tiagocreator.com>
2023-01-25 08:58:56 -0300

    Fix footer source link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,12 @@ import { i18n } from '../locale/i18n';
 const Footer = () => {
   return (
     <footer className="w-full h-8 2xl:h-9 bg-[#262629] text-theme-white flex justify-center items-center shadow-[0px_-1px_10px_0px] shadow-[rgba(0,0,0,0.2)]">
-      <span className="text-sm font-medium">2020 Tiago Leite</span>{' '}
+      <span className="text-sm font-medium">2023 Tiago Leite</span>{' '}
       <a
         className="text-sm absolute right-2.5"
-        href="/"
+        href="https://github.com/tiagocreator/my-portfolio"
         target="_blank"
-        rel="nofererrer"
+        rel="noreferrer"
       >
         {i18n.t('footer')}
       </a>

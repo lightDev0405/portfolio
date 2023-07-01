4c05f1d72a7d48d67290eb111ee99f80157ad6c7
Tiago Leite <contato@tiagocreator.com>
2023-07-01 15:47:23 -0300

    Change skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,10 +3,10 @@ import {
   SiFirebase,
   SiReact,
   SiFigma,
-  SiExpress,
-  SiMongodb,
   SiRedux,
+  SiJavascript,
   SiTypescript,
+  SiBootstrap,
 } from 'react-icons/si';
 import { DiGit } from 'react-icons/di';
 import { BsPlusLg, BsServer } from 'react-icons/bs';
@@ -37,21 +37,14 @@ const Skills = () => {
             <span className='mt-4 text-sm 2xl:text-base'>React</span>
           </div>
           <div className='shadow-md shadow-[rgba(19,33,50,0.8)] p-5 hover:scale-[1.2] duration-500 select-none'>
-            <FaNodeJs className='text-[#8BBF3D] mx-auto mb-4 text-4xl xl:text-5xl 2xl:text-6xl' />
-            <span className='mt-4 text-sm 2xl:text-base'>NodeJs</span>
+            <SiRedux className='text-[#764ABC] mx-auto mb-4 text-4xl xl:text-5xl 2xl:text-6xl' />
+            <span className='mt-4 text-sm 2xl:text-base'>Redux</span>
           </div>
           <div className='shadow-md shadow-[rgba(19,33,50,0.8)] p-5 hover:scale-[1.2] duration-500 select-none'>
-            <SiExpress className='text-[#EEEEEE] mx-auto mb-4 text-4xl xl:text-5xl 2xl:text-6xl' />
-            <span className='mt-4 text-sm 2xl:text-base'>Express</span>
+            <SiJavascript className='text-[#EFD81E] mx-auto mb-4 text-4xl xl:text-5xl 2xl:text-6xl' />
+            <span className='mt-4 text-sm 2xl:text-base'>JavaScript</span>
           </div>
           <div className='shadow-md shadow-[rgba(19,33,50,0.8)] p-5 hover:scale-[1.2] duration-500 select-none'>
-            <SiMongodb className='text-[#0FA54D] mx-auto mb-4 text-4xl xl:text-5xl 2xl:text-6xl' />
-            <span className='mt-4 text-sm 2xl:text-base'>MongoDB</span>
-          </div>
-          <div
-            className={`${
-              active ? 'block' : 'hidden'
-            } sm:block shadow-md shadow-[rgba(19,33,50,0.8)] p-5 hover:scale-[1.2] duration-500 select-none`}>
             <SiTypescript className='text-[#146BBC] mx-auto mb-4 text-4xl xl:text-5xl 2xl:text-6xl' />
             <span className='mt-4 text-sm 2xl:text-base'>TypeScript</span>
           </div>
@@ -59,8 +52,36 @@ const Skills = () => {
             className={`${
               active ? 'block' : 'hidden'
             } sm:block shadow-md shadow-[rgba(19,33,50,0.8)] p-5 hover:scale-[1.2] duration-500 select-none`}>
-            <SiRedux className='text-[#764ABC] mx-auto mb-4 text-4xl xl:text-5xl 2xl:text-6xl' />
-            <span className='mt-4 text-sm 2xl:text-base'>Redux</span>
+            <FaSass className='scale-[1.4] text-[#C66394] mx-auto mb-4 text-4xl xl:text-5xl 2xl:text-6xl' />
+            <span className='mt-4 text-sm 2xl:text-base'>Sass</span>
+          </div>
+          <div
+            className={`${
+              active ? 'block' : 'hidden'
+            } sm:block shadow-md shadow-[rgba(19,33,50,0.8)] p-[20px] hover:scale-[1.2] duration-500 select-none`}>
+            <SiTailwindcss className='text-[#36B7F0] mx-auto mb-4 text-4xl xl:text-5xl 2xl:text-6xl' />
+            <span className='mt-4 text-sm 2xl:text-base'>Tailwind CSS</span>
+          </div>
+          <div
+            className={`${
+              active ? 'block' : 'hidden'
+            } sm:block shadow-md shadow-[rgba(19,33,50,0.8)] p-5 hover:scale-[1.2] duration-500 select-none`}>
+            <SiBootstrap className='text-[#8715F2] mx-auto mb-4 text-4xl xl:text-5xl 2xl:text-6xl' />
+            <span className='mt-4 text-sm 2xl:text-base'>Bootstrap</span>
+          </div>
+          <div
+            className={`${
+              active ? 'block' : 'hidden'
+            } sm:block shadow-md shadow-[rgba(19,33,50,0.8)] p-5 hover:scale-[1.2] duration-500 select-none`}>
+            <SiFigma className='text-[#62CA80] mx-auto mb-4 text-4xl xl:text-5xl 2xl:text-6xl' />
+            <span className='mt-4 text-sm 2xl:text-base'>Figma</span>
+          </div>
+          <div
+            className={`${
+              active ? 'block' : 'hidden'
+            } sm:block shadow-md shadow-[rgba(19,33,50,0.8)] p-5 hover:scale-[1.2] duration-500 select-none`}>
+            <FaNodeJs className='text-[#8BBF3D] mx-auto mb-4 text-4xl xl:text-5xl 2xl:text-6xl' />
+            <span className='mt-4 text-sm 2xl:text-base'>NodeJs</span>
           </div>
           <div
             className={`${
@@ -82,27 +103,6 @@ const Skills = () => {
             } sm:block shadow-md shadow-[rgba(19,33,50,0.8)] p-5 hover:scale-[1.2] duration-500 select-none`}>
             <DiGit className='scale-[1.4] text-[#e44c30] mx-auto mb-4 text-4xl xl:text-5xl 2xl:text-6xl' />
             <span className='mt-4 text-sm 2xl:text-base'>Git</span>
-          </div>
-          <div
-            className={`${
-              active ? 'block' : 'hidden'
-            } sm:block shadow-md shadow-[rgba(19,33,50,0.8)] p-5 hover:scale-[1.2] duration-500 select-none`}>
-            <FaSass className='scale-[1.4] text-[#C66394] mx-auto mb-4 text-4xl xl:text-5xl 2xl:text-6xl' />
-            <span className='mt-4 text-sm 2xl:text-base'>Sass</span>
-          </div>
-          <div
-            className={`${
-              active ? 'block' : 'hidden'
-            } sm:block shadow-md shadow-[rgba(19,33,50,0.8)] p-[20px] hover:scale-[1.2] duration-500 select-none`}>
-            <SiTailwindcss className='text-[#36B7F0] mx-auto mb-4 text-4xl xl:text-5xl 2xl:text-6xl' />
-            <span className='mt-4 text-sm 2xl:text-base'>Tailwind CSS</span>
-          </div>
-          <div
-            className={`${
-              active ? 'block' : 'hidden'
-            } sm:block shadow-md shadow-[rgba(19,33,50,0.8)] p-5 hover:scale-[1.2] duration-500 select-none`}>
-            <SiFigma className='text-[#62CA80] mx-auto mb-4 text-4xl xl:text-5xl 2xl:text-6xl' />
-            <span className='mt-4 text-sm 2xl:text-base'>Figma</span>
           </div>
         </div>
         <button

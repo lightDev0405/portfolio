36cb48ca01121460a3e5859e007eea1f64d9a1cf
Tiago Leite <contato@tiagocreator.com>
2023-07-01 11:32:55 -0300

    Update about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,24 +8,22 @@ import { TypeAnimation } from 'react-type-animation';
 
 const About = () => {
   return (
-    <section className="w-full flex flex-col pt-14 md:pt-20" name="about">
+    <section className='w-full flex flex-col pt-14 md:pt-20' name='about'>
       <SolarSystem />
-      <div className="w-full max-w-[900px] flex flex-col justify-center mx-auto px-4 md:px-8">
-        <div className="flex flex-col md:flex-row items-start justify-between mb-6">
+      <div className='w-full max-w-[900px] flex flex-col justify-center mx-auto px-4 md:px-8'>
+        <div className='flex flex-col md:flex-row items-start justify-between mb-6'>
           {/* Introduction */}
-          <div className="md:mt-6 mx-auto md:mx-0">
+          <div className='md:mt-6 mx-auto md:mx-0'>
             <h1 className="flex items-center gap-2 text-2xl sm:text-3xl 2xl:text-4xl font-bold after:content-[''] after:w-1.5 after:h-14 after:bg-theme-blue-100 after:animate-cursorBlink">
               {i18n.t('aboutSection.intro')}
             </h1>
-            <h2 className="text-base sm:text-lg mb-3">
-              {i18n.t('aboutSection.welcome')}
-            </h2>
-            <div className="text-base sm:text-xl 2xl:text-2xl flex items-center justify-start md:mt-5">
+            <h2 className='text-base sm:text-lg mb-3'>{i18n.t('aboutSection.welcome')}</h2>
+            <div className='text-base sm:text-xl 2xl:text-2xl flex items-center justify-start md:mt-5'>
               <p>{i18n.t('aboutSection.smallDescription')} </p>
-              <span className="text-3xl sm:text-4xl 2xl:text-5xl text-theme-blue-50 ml-1 md:ml-2 md:mr-1">
+              <span className='text-3xl sm:text-4xl 2xl:text-5xl text-theme-blue-50 ml-1 md:ml-2 md:mr-1'>
                 [
               </span>
-              <div className="text-sm sm:text-lg md:text-xl 2xl:text-2xl font-medium">
+              <div className='text-sm sm:text-lg md:text-xl 2xl:text-2xl font-medium'>
                 <TypeAnimation
                   cursor={false}
                   sequence={[
@@ -41,57 +39,52 @@ const About = () => {
                     3000,
                     `${i18n.t('aboutSection.animatedTexts.textSix')}`,
                     3000,
-                    `${i18n.t('aboutSection.animatedTexts.textSeven')}`,
-                    3000,
                   ]}
-                  wrapper="span"
-                  speed="45"
+                  wrapper='span'
+                  speed='45'
                   repeat={Infinity}
                 />
               </div>
-              <span className="text-3xl sm:text-4xl 2xl:text-5xl text-theme-blue-50 sm:ml-1">
+              <span className='text-3xl sm:text-4xl 2xl:text-5xl text-theme-blue-50 sm:ml-1'>
                 ]
               </span>
             </div>
           </div>
           {/* Profile Picture */}
-          <div className="w-40 h-40 md:min-w-[180px] md:min-h-[180px] rounded-full border-2 md:border-[3px] border-theme-white flex justify-center items-center mt-6 md:ml-10 mx-auto md:mx-0">
+          <div className='w-40 h-40 md:min-w-[180px] md:min-h-[180px] rounded-full border-2 md:border-[3px] border-theme-white flex justify-center items-center mt-6 md:ml-10 mx-auto md:mx-0'>
             <img
               src={ProfilePicture}
-              alt="tiago leite selfie"
-              className="block w-36 h-36 hover:scale-105 active:scale-105 md:min-w-[160px] md:min-h-[160px] rounded-full border border-solid border-black duration-300 select-none"
-            ></img>
+              alt='tiago leite selfie'
+              className='block w-36 h-36 hover:scale-105 active:scale-105 md:min-w-[160px] md:min-h-[160px] rounded-full border border-solid border-black duration-300 select-none'></img>
           </div>
         </div>
-        <div className="md:hidden mx-auto md:mx-0">
-          <h1 className="text-2xl md:text-3xl border-b-[4px] inline-block border-theme-white mb-4 pb-1">
+        <div className='md:hidden mx-auto md:mx-0'>
+          <h1 className='text-2xl md:text-3xl border-b-[4px] inline-block border-theme-white mb-4 pb-1'>
             {i18n.t('aboutSection.aboutMe')}
           </h1>
         </div>
         {/* Description */}
-        <p className="text-base sm:text-lg 2xl:text-xl">
-          {i18n.t('aboutSection.description')}
-        </p>
+        <p className='text-base sm:text-lg 2xl:text-xl'>{i18n.t('aboutSection.description')}</p>
 
         {/* Buttons */}
-        <div className="md:mx-0 w-full flex justify-center md:justify-start items-center mt-6 text-theme-white text-sm sm:text-base">
-          <Link to="works" smooth={true} duration={600}>
-            <button className="w-28 2xl:w-32 h-12 flex justify-center items-center rounded bg-transparent mr-4 md:mr-7 border-2 hover:text-white active:text-white hover:bg-theme-blue-50 active:bg-theme-blue-50 hover:border-theme-blue-50 active:border-theme-blue-50 duration-200 group font-medium">
+        <div className='md:mx-0 w-full flex justify-center md:justify-start items-center mt-6 text-theme-white text-sm sm:text-base'>
+          <Link to='works' smooth={true} duration={600}>
+            <button className='w-28 2xl:w-32 h-12 flex justify-center items-center rounded bg-transparent mr-4 md:mr-7 border-2 hover:text-white active:text-white hover:bg-theme-blue-50 active:bg-theme-blue-50 hover:border-theme-blue-50 active:border-theme-blue-50 duration-200 group font-medium'>
               {i18n.t('aboutSection.buttons.work')}
-              <span className="group-hover:rotate-90 group-active:rotate-90 duration-300">
-                <HiArrowNarrowRight className="ml-1" />
+              <span className='group-hover:rotate-90 group-active:rotate-90 duration-300'>
+                <HiArrowNarrowRight className='ml-1' />
               </span>
             </button>
           </Link>
-          <Link to="contact" smooth={true} duration={600}>
-            <button className="w-24 2xl:w-28 h-12 flex justify-center items-center rounded bg-theme-blue-50 hover:bg-theme-blue-100 active:bg-theme-blue-100 hover:text-white active:text-white duration-200 font-medium">
+          <Link to='contact' smooth={true} duration={600}>
+            <button className='w-24 2xl:w-28 h-12 flex justify-center items-center rounded bg-theme-blue-50 hover:bg-theme-blue-100 active:bg-theme-blue-100 hover:text-white active:text-white duration-200 font-medium'>
               {i18n.t('aboutSection.buttons.hire')}
             </button>
           </Link>
         </div>
       </div>
       {/* Sidebar */}
-      <div className="hidden md:block lg:hidden absolute top-40 left-0">
+      <div className='hidden md:block lg:hidden absolute top-40 left-0'>
         <Sidebar />
       </div>
     </section>

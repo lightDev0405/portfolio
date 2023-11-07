75ec0c380b9bbfa5321b0a6af7a80139b9d85b53
Tiago Leite <contato@tiagocreator.com>
2023-11-07 08:56:21 -0300

    Add home component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,4 @@
+import Home from './Home';
 import About from './About';
 import Skills from './Skills';
 import Works from './Works';
@@ -5,7 +6,8 @@ import Contact from './Contact';
 
 const Main = () => {
   return (
-    <main className="bg-theme-black text-theme-white">
+    <main className='bg-theme-black text-theme-white'>
+      <Home />
       <About />
       <Skills />
       <Works />

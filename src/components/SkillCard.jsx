4c543a46c812b83d7d974a43198d9b308d3a3241
Tiago Leite <contato@tiagocreator.com>
2023-11-13 14:20:13 -0300

    Create skill card component

---
+++ src/components/SkillCard.jsx
@@ -0,0 +1,18 @@
+const SkillCard = ({ shadow, color, icon, name, active = true, iconStyles = undefined }) => {
+  const DynamicIcon = icon;
+  const iconCustomClass = iconStyles;
+
+  return (
+    <div
+      className={`${
+        active ? 'block' : 'hidden sm:block'
+      } shadow-md shadow-[${shadow}] p-2 hover:scale-[1.1] duration-500 select-none`}>
+      <DynamicIcon
+        className={`text-[${color}] mx-auto mb-4 text-4xl xl:text-5xl 2xl:text-6xl ${iconCustomClass}`}
+      />
+      <span className='mt-4 text-sm 2xl:text-base'>{name}</span>
+    </div>
+  );
+};
+
+export default SkillCard;

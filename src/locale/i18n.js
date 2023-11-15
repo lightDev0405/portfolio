d42417a2e01d9bf37acda47fbec2b94d85a297f7
Tiago Leite <contato@tiagocreator.com>
2023-11-14 21:29:44 -0300

    Change default website locale

--- src/locale/i18n.js
+++ src/locale/i18n.js
@@ -2,10 +2,14 @@ import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { content } from './languages';
 
+if (!localStorage.getItem('i18nextLng')) {
+  localStorage.setItem('i18nextLng', 'en-US');
+}
+
 i18n.use(LanguageDetector).init({
   debug: false,
   defaultNS: ['locale'],
-  fallbackLng: 'pt',
+  fallbackLng: 'en',
   ns: 'locale',
   resources: content,
 });

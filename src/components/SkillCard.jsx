a2a5a9db468d9d4906d0dab2444792f99c75bd51
Tiago Leite <contato@tiagocreator.com>
2023-11-16 21:04:13 -0300

    Update skill card styles

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -1,16 +1,12 @@
-const SkillCard = ({ shadow, color, icon, name, active = true, iconStyles = undefined }) => {
+const SkillCard = ({ color, icon, name, active = true, iconStyles = undefined }) => {
   const DynamicIcon = icon;
-  const iconCustomClass = iconStyles;
 
   return (
     <div
       className={`${
         active ? 'block' : 'hidden sm:block'
-      } shadow-md shadow-[${shadow}] p-2 hover:scale-[1.1] duration-500 select-none`}>
-      <DynamicIcon
-        className={`mx-auto mb-4 text-4xl xl:text-5xl 2xl:text-6xl ${iconCustomClass}`}
-        style={{ color }}
-      />
+      } shadow-md shadow-[rgba(19,33,50,0.8)] p-2 hover:scale-[1.1] duration-500 select-none`}>
+      <DynamicIcon className={`mx-auto mb-4 text-4xl xl:text-5xl 2xl:text-6xl`} style={{ color }} />
       <span className='mt-4 text-sm 2xl:text-base'>{name}</span>
     </div>
   );

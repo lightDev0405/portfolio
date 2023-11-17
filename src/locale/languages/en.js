e4e7ccaeda36c64e4b4c4f4982d0b3a1aba152fb
Tiago Leite <contato@tiagocreator.com>
2023-11-17 14:47:08 -0300

    Update english locale

--- src/locale/languages/en.js
+++ src/locale/languages/en.js
@@ -61,16 +61,10 @@ const content = {
             'Database of movies mimicking the look of Netflix, it has integration with Firebase to create an account, login and save the movies to favorites.',
           alt: 'cineflix website of movie and series information',
         },
-        classicGames: {
-          name: 'Classic Games',
-          description:
-            'Website of my favorite classic games that I recreated in a more simplified way using HTML, CSS and JavaScript. My goal with this is to learn the logic and the syntax of JavaScript.',
-          alt: 'classic games repository website',
-        },
         noir: {
           name: 'Noir',
           description:
-            'Noir is a 100% responsive barbershop website, with dark-mode and 4 pages made in HTML, LESS and JavaScript, without the use of any frameworks.',
+            'Noir is a website with a attractive, sophisticated and functional design, dedicated to offering a complete barbershop experience.',
           alt: 'noir barbershop website',
         },
         lasso: {
@@ -79,17 +73,17 @@ const content = {
             'A complete e-commerce where users can create an account and login, add products to cart, shop, filter products by category, name, brand, see the order list and much more.',
           alt: 'lasso online store',
         },
-        hypeCamper: {
-          name: 'Hype Camper',
+        casaECor: {
+          name: 'Casa&Cor',
           description:
-            'Hype Camper is a full stack CRUD website where Node, Express and MongoDB have been integrated into the front-end. Users can create, review and manage campgrounds.',
-          alt: 'hype camper camping locations website',
+            'An elegant renovation and decoration website with an intuitive, harmonious and responsive design. Features a visual gallery, descriptive information, and dark mode.',
+          alt: 'renovation and decoration website',
         },
       },
       contactSection: {
         sectionName: 'Contact',
         intro:
-          'To contact me, fill out the form below, or if you prefer, contact me via LinkedIn or send an email. I am available for hire and open to any ideas of cooperation, also as a freelancer.',
+          'To contact me, fill out the form below, or if you prefer, contact me via LinkedIn or send an email. I am available for hire and open to any ideas of cooperation.',
         placeholders: {
           placeholderName: 'Name',
           placeholderMessage: 'Message',

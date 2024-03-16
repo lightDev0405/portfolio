1a146f84d1a8a639c1a11f0f8ebea0685f234666
Tiago Leite <contato@tiagocreator.com>
2024-03-16 10:02:15 -0300

    Refactor work blocks logic

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -23,6 +23,41 @@ const Works = () => {
     arrows: true,
   };
 
+  let info = {
+    casaECor: {
+      alt: i18n.t('worksSection.casaECor.alt'),
+      name: `${i18n.t('worksSection.casaECor.name')}`,
+      description: `${i18n.t('worksSection.casaECor.description')}`,
+      tools: 'HTML5, LESS, JavaScript',
+      link: 'https://casa-e-cor.vercel.app/',
+      code: 'https://github.com/tiagocreator/casa-e-cor',
+    },
+    lasso: {
+      alt: i18n.t('worksSection.lasso.alt'),
+      name: `${i18n.t('worksSection.lasso.name')}`,
+      description: `${i18n.t('worksSection.lasso.description')}`,
+      tools: 'React, Redux, Sass',
+      link: 'https://lasso-ecommerce.up.railway.app/',
+      code: 'https://github.com/tiagocreator/lasso-ecommerce',
+    },
+    noir: {
+      alt: i18n.t('worksSection.noir.alt'),
+      name: `${i18n.t('worksSection.noir.name')}`,
+      description: `${i18n.t('worksSection.noir.description')}`,
+      tools: 'HTML5, CSS3, JavaScript',
+      link: 'https://noir-barbearia.vercel.app/',
+      code: 'https://github.com/tiagocreator/noir-barbearia',
+    },
+    alugaFacil: {
+      alt: i18n.t('worksSection.alugaFacil.alt'),
+      name: `${i18n.t('worksSection.alugaFacil.name')}`,
+      description: `${i18n.t('worksSection.alugaFacil.description')}`,
+      tools: 'React, TypeScript, Sass',
+      link: 'https://aluga-facil.vercel.app/',
+      code: 'https://github.com/tiagocreator/aluga-facil',
+    },
+  };
+
   return (
     <section name='works' className='pt-12 md:pt-20 w-full text-theme-white'>
       <div className='flex flex-col w-full h-full max-w-[900px] mx-auto py-4 px-8'>
@@ -38,89 +73,97 @@ const Works = () => {
           <div className='mx-auto w-full max-w-[400px] md:hidden'>
             <Slider {...settings}>
               <div>
+                {/* Casa & Cor */}
                 <WorkElement
                   img={CasaECorMobileImg}
-                  alt={i18n.t('worksSection.casaECor.alt')}
-                  name={`${i18n.t('worksSection.casaECor.name')}`}
-                  description={`${i18n.t('worksSection.casaECor.description')}`}
-                  tools='HTML5, LESS, JavaScript'
-                  link='https://casa-e-cor.vercel.app/'
-                  code='https://github.com/tiagocreator/casa-e-cor'
+                  alt={info.casaECor.alt}
+                  name={info.casaECor.name}
+                  description={info.casaECor.description}
+                  tools={info.casaECor.tools}
+                  link={info.casaECor.link}
+                  code={info.casaECor.code}
                 />
               </div>
               <div>
+                {/* Lasso */}
                 <WorkElement
                   img={LassoMobileImg}
-                  alt={i18n.t('worksSection.lasso.alt')}
-                  name={`${i18n.t('worksSection.lasso.name')}`}
-                  description={`${i18n.t('worksSection.lasso.description')}`}
-                  tools='React, Redux, Sass'
-                  link='https://lasso-ecommerce.up.railway.app/'
-                  code='https://github.com/tiagocreator/lasso-ecommerce'
+                  alt={info.lasso.alt}
+                  name={info.lasso.name}
+                  description={info.lasso.description}
+                  tools={info.lasso.tools}
+                  link={info.lasso.link}
+                  code={info.lasso.code}
                 />
               </div>
               <div>
+                {/* Noir */}
                 <WorkElement
                   img={NoirMobileImg}
-                  alt={i18n.t('worksSection.noir.alt')}
-                  name={`${i18n.t('worksSection.noir.name')}`}
-                  description={`${i18n.t('worksSection.noir.description')}`}
-                  tools='HTML5, CSS3, JavaScript'
-                  link='https://noir-barbearia.vercel.app/'
-                  code='https://github.com/tiagocreator/noir-barbearia'
+                  alt={info.noir.alt}
+                  name={info.noir.name}
+                  description={info.noir.description}
+                  tools={info.noir.tools}
+                  link={info.noir.link}
+                  code={info.noir.code}
                 />
               </div>
               <div>
+                {/* Aluga Fácil */}
                 <WorkElement
                   img={AlugaFacilMobileImg}
-                  alt={i18n.t('worksSection.alugaFacil.alt')}
-                  name={`${i18n.t('worksSection.alugaFacil.name')}`}
-                  description={`${i18n.t('worksSection.alugaFacil.description')}`}
-                  tools='React, TypeScript, Sass'
-                  link='https://aluga-facil-ten.netlify.app/'
-                  code='https://github.com/tiagocreator/aluga-facil'
+                  alt={info.alugaFacil.alt}
+                  name={info.alugaFacil.name}
+                  description={info.alugaFacil.description}
+                  tools={info.alugaFacil.tools}
+                  link={info.alugaFacil.link}
+                  code={info.alugaFacil.code}
                 />
               </div>
             </Slider>
           </div>
           {/* Desktop */}
           <div className='hidden md:flex flex-col'>
+            {/* Casa & Cor */}
             <WorkElement
               img={CasaECorDesktopImg}
-              alt={i18n.t('worksSection.casaECor.alt')}
-              name={`${i18n.t('worksSection.casaECor.name')}`}
-              description={`${i18n.t('worksSection.casaECor.description')}`}
-              tools='HTML5, LESS, JavaScript'
-              link='https://casa-e-cor.vercel.app/'
-              code='https://github.com/tiagocreator/casa-e-cor'
+              alt={info.casaECor.alt}
+              name={info.casaECor.name}
+              description={info.casaECor.description}
+              tools={info.casaECor.tools}
+              link={info.casaECor.link}
+              code={info.casaECor.code}
             />
+            {/* Lasso */}
             <WorkElement
               img={LassoDesktopImg}
-              alt={i18n.t('worksSection.lasso.alt')}
-              name={`${i18n.t('worksSection.lasso.name')}`}
-              description={`${i18n.t('worksSection.lasso.description')}`}
-              tools='React, Redux, Sass'
-              link='https://lasso-ecommerce.up.railway.app/'
-              code='https://github.com/tiagocreator/lasso-ecommerce'
+              alt={info.lasso.alt}
+              name={info.lasso.name}
+              description={info.lasso.description}
+              tools={info.lasso.tools}
+              link={info.lasso.link}
+              code={info.lasso.code}
               reverse
             />
+            {/* Noir */}
             <WorkElement
               img={NoirDesktopImg}
-              alt={i18n.t('worksSection.noir.alt')}
-              name={`${i18n.t('worksSection.noir.name')}`}
-              description={`${i18n.t('worksSection.noir.description')}`}
-              tools='HTML5, CSS3, JavaScript'
-              link='https://noir-barbearia.vercel.app/'
-              code='https://github.com/tiagocreator/noir-barbearia'
+              alt={info.noir.alt}
+              name={info.noir.name}
+              description={info.noir.description}
+              tools={info.noir.tools}
+              link={info.noir.link}
+              code={info.noir.code}
             />
+            {/* Aluga Fácil */}
             <WorkElement
               img={AlugaFacilDesktopImg}
-              alt={i18n.t('worksSection.alugaFacil.alt')}
-              name={`${i18n.t('worksSection.alugaFacil.name')}`}
-              description={`${i18n.t('worksSection.alugaFacil.description')}`}
-              tools='React, TypeScript, Sass'
-              link='https://aluga-facil-ten.netlify.app/'
-              code='https://github.com/tiagocreator/aluga-facil'
+              alt={info.alugaFacil.alt}
+              name={info.alugaFacil.name}
+              description={info.alugaFacil.description}
+              tools={info.alugaFacil.tools}
+              link={info.alugaFacil.link}
+              code={info.alugaFacil.code}
               reverse
             />
           </div>
